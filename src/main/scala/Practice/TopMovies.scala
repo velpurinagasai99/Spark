9abb1e66fca71f08a8605224645bcd325ff3c4ab
velpurinagasai99 <velpurinagasai99@gmail.com>
2025-03-04 17:47:58 -0500

    Directly Source File to BroadCast Variable. Difference in DAGS for 2RDD's and 1RDD with a broadcast variable.

--- src/main/scala/Practice/TopMovies.scala
+++ src/main/scala/Practice/TopMovies.scala
@@ -2,6 +2,9 @@ package Practice
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
+import org.apache.spark.broadcast.Broadcast
+
+import scala.io.Source
 
 //******Use versions related to spark and scala-1.0 when running this file*********
 
@@ -14,16 +17,21 @@ object TopMovies extends App {
   val moviesWithMoreRatings = idRatingsCountinFloat.filter(x=>x._2._2>100)
   val avgMovieRating = moviesWithMoreRatings.map(x=>(x._1,(x._2._1/x._2._2).toFloat))
   val topMovies = avgMovieRating.filter(x=>(x._2>4.3))
-  val bcvar = sc.broadcast(topMovies.collect.toMap)
-  val moviesData = sc.textFile("C:/Users/velpu/Documents/BigDataTrendyTech/week-11/movies-201019-002101.dat")
-  val splittedMoviesData = moviesData.map(x=>(x.split("::")(0),x.split("::")(2)))
+//  val bcvar = sc.broadcast(topMovies.collect.toMap)
+  val filepath = Source.fromFile("C:/Users/velpu/Documents/BigDataTrendyTech/week-11/movies-201019-002101.dat","ISO-8859-1")
+  val movieMap = filepath.getLines().map { line =>
+      val parts = line.split("::")
+      parts(0) -> parts(1)  // Convert movieId to Int and take title
+    }.toMap
 
-  val joinedMap = splittedMoviesData.flatMap { case(key, value) =>
-    bcvar.value.get(key).map { otherValue =>
+  // Broadcast the map
+  val broadcastMovieMap: Broadcast[Map[String, String]] = sc.broadcast(movieMap)
+  val joinedMap = topMovies.flatMap { case(key, value) =>
+    broadcastMovieMap.value.get(key).map { otherValue =>
       (key, (value, otherValue))
     }
   }
-  val topMoviesNames=joinedMap.map(x=>x._2._1)
+  val topMoviesNames=joinedMap.map(x=>x._2._2)
   joinedMap.collect.foreach(println)
   scala.io.StdIn.readLine()
 }
