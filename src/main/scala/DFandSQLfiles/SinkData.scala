490b983be0dd85856f555a8faafafe7e453955cf
Rahitya Velpuri <rahitya569@gmail.com>
2025-04-01 14:48:54 -0400

    Adding Data to Target location *****Hadoop needs to be installaed locally to create files******

---
+++ src/main/scala/DFandSQLfiles/SinkData.scala
@@ -0,0 +1,28 @@
+package DFandSQLfiles
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+object SinkData extends App{
+  var sparkConf = new SparkConf()
+  sparkConf.set("spark.app.name","spark session application")
+  sparkConf.set("spark.master","local[*]")
+
+  val spark =SparkSession.builder()
+    .config(sparkConf)                // instead of sparkConf declaration we can directly use
+    .getOrCreate()
+
+  val ordersDF = spark.read
+    .format("csv")
+    .option("header",true)
+    .option("inferschema",true)
+    .option("path","C:/Users/rahit/OneDrive/Desktop/BigData/Week-12/orders-201025-223502.csv")
+    .load()
+  ordersDF.write
+    .format("csv")
+    .mode(SaveMode.Overwrite)
+    .option("path","C:/Users/rahit/OneDrive/Desktop/BigData/Week-12/outputs")
+    .save()
+  spark.stop()
+
+}
