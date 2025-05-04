07c591140642067c712867bcd1304045d0441ff2
velpurinagasai99 <velpurinagasai99@gmail.com>
2025-05-04 18:06:46 -0400

    rollback to below commit if regex is not working, for now it is....Not sure might be hadoop not installed properly previously

--- src/main/scala/DFandSQLfiles/regexOrderStatus.scala
+++ src/main/scala/DFandSQLfiles/regexOrderStatus.scala
@@ -1,12 +1,16 @@
 package DFandSQLfiles
 
-import DFandSQLfiles.DFExample.spark
-import DFandSQLfiles.Datasets.spark
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.SparkSession
 case class Orders(order_id:Int,customer_id:Int, order_status:String)
 
 object Helper extends Serializable {
+
+  }
+
+
+object regexOrdersStatus extends App {
+
   val myregex="""^(\S+) (\S+)\t(\S+)\,(\S+)""".r
   def parser(line: String)={
     line match {
@@ -14,11 +18,6 @@ object Helper extends Serializable {
         Orders(order_id.toInt , customer_id.toInt , order_status )
     }
   }
-  }
-
-
-object regexOrdersStatus extends App {
-
   val sparkConf = new SparkConf()
   sparkConf.set("spark.app.name", "spark session application")
   sparkConf.set("spark.master", "local[*]")
@@ -27,10 +26,10 @@ object regexOrdersStatus extends App {
     .config(sparkConf)
     .getOrCreate()
 
-  val lines = spark.sparkContext.textFile("C:/Users/rahit/OneDrive/Desktop/BigData/Week-12/Orders1.csv")
+  val lines = spark.sparkContext.textFile("C:/Users/velpu/Documents/BigDataTrendyTech/week-11/Orders.txt")
 
   import spark.implicits._
-  val OrdersDS = lines.map(x=>Helper.parser(x)).toDF()
+  val OrdersDS = lines.map(x=>parser(x)).toDF()
   OrdersDS.show()
   //  OrdersDS.groupBy("order_status").count().show
 }
