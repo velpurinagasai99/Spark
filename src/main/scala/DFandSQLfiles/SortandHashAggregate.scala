54d84dd3bfd48fe29c01ba90921d81d413f2d2c6
velpurinagasai99 <velpurinagasai99@gmail.com>
2025-05-12 17:48:37 -0400

    Sort Aggregate and Hash Aggregate

---
+++ src/main/scala/DFandSQLfiles/SortandHashAggregate.scala
@@ -0,0 +1,37 @@
+package DFandSQLfiles
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+object SortandHashAggregate extends App{
+  val sparkConf = new SparkConf()
+  sparkConf.set("spark.app.name","spark session application")
+  sparkConf.set("spark.master","local[*]")
+
+  val spark =SparkSession.builder()
+    .config(sparkConf)
+    .getOrCreate()
+
+  val ordersDF : Dataset[Row] = spark.read
+    .format("csv")
+    .option("header",true)
+    .option("inferSchema",true)
+    .option("path","C:/Users/velpu/Documents/BigDataTrendyTech/week-12/Orders.csv")
+    .load
+
+  ordersDF.createOrReplaceTempView("orders")
+
+  //Results Sort Aggregate
+  spark.sql("select order_customer_id,date_format(order_date,'MMMM') orderdt,count(1) cnt, first(date_format(" +
+    "order_date,'M')) monthnum from orders group by order_customer_id, orderdt, monthnum").explain           //Sumit sir explained sort aggregate and Hash aggregate is something to do with strings but not with order by.But, it is something to do with order by of strings
+
+  //Results Hash Aggregate
+  spark.sql("select order_customer_id,date_format(order_date,'MMMM') orderdt,count(1) cnt, date_format(" +
+    "order_date,'M') monthnum from orders group by order_customer_id, orderdt, monthnum").explain           //This leads to Hash Aggregate instead Sort Aggregate as first needs to sort but not group by
+
+  //Results Hash Aggregate
+  spark.sql("select order_customer_id,date_format(order_date,'MMMM') orderdt,count(1) cnt, cast(first(date_format(" +
+    "order_date,'M')) as int) monthnum from orders group by order_customer_id, orderdt").explain           //This leads to Hash Aggregate as integers can be done Hash in Hashing but not strings.
+
+  spark.stop()
+}
