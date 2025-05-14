49220ade6c0350babeb6f8204091fdf88ec7f960
velpurinagasai99 <velpurinagasai99@gmail.com>
2025-05-14 19:13:05 -0400

    StateFull Transformation Window streaming

--- src/main/scala/Streaming/wCStatefull.scala
+++ src/main/scala/Streaming/wCStatefull.scala
@@ -5,9 +5,12 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /*
 **************************************   Analyze Dags, Super Interesting   ************************************************
+*
+* reduceByWindow is worked on single value where as reduceByKeyAndWindow works on Key, Value Pairs, remaining everything is same
+* countByWindow counts the number of lines we have typed(accepts only two parameters window, slide size)...that's it
 */
 
-object entireStateFull extends App{
+object wCStatefull extends App{
 
   def updatefunc(newValues:Seq[Int],previousState:Option[Int]):Option[Int]={
     val newCount = previousState.getOrElse(0)+newValues.sum
@@ -19,8 +22,9 @@ object entireStateFull extends App{
   val ssc = new StreamingContext(sc, Seconds(10))
   val lines = ssc.socketTextStream("localhost",9998)          //in cmd run: ncat -lk 9998
   ssc.checkpoint(".")
-  val words = lines.flatMap(x=>x.split(" ")).map(x=>(x,1))    //open googlechrome and go localhost:9998 and then start typing in
-  val wordscount = words.updateStateByKey(updatefunc)              //command Prompt
+  val words = lines.flatMap(x=>x.split(" ")).map(x=>(x,1))    //open googlechrome and go localhost:9998 and then start typing in command Prompt
+
+  val wordscount = words.updateStateByKey(updatefunc)    //words.reduceByKeyAndWindow((x,y)=>x+y,(x,y)=>x-y,Seconds(30),Seconds(20)) //Window streaming
   wordscount.print()
   ssc.start()
   ssc.awaitTermination()
