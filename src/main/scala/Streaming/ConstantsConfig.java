f3677f01b78782f069ed27809f9c1fc71cae2dc0
velpurinagasai99 <velpurinagasai99@gmail.com>
2025-07-02 14:58:04 -0400

    First we are producing data and consuming and segrigating data and then producing again

---
+++ src/main/scala/Streaming/ConstantsConfig.java
@@ -0,0 +1,9 @@
+
+public class ConstantsConfig {
+
+    final static String appId = "id-1";
+    final static String bootstrapId = "localhost:9092,localhost:9093";
+    final static String topicname = "all_orders";
+
+
+}
